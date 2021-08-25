fc69295b6931d1d2f754222bef08db90532b06cd
sudhanya <rsudhanya@gmail.com>
2021-08-25 23:22:09 +0530

    Bug fixed

--- pages/meetups/[meetupId]/index.js
+++ pages/meetups/[meetupId]/index.js
@@ -63,8 +63,7 @@ let MeetupDetails = (props) => {
 
 export const getStaticPaths = async () => {
   const data = await getMeetupIds();
-  console.log(data);
-  console.log(typeof data);
+
   const paths = data.payload.map((item) => ({
     params: {
       meetupId: item,
